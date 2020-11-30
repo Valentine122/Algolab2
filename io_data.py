b25c2ba5415b87276d8d3699105f2626b7696faf
Valentine122 <56410350+Valentine122@users.noreply.github.com>
2020-11-30 16:01:34 +0200

    Add files via upload

---
+++ io_data.py
@@ -0,0 +1,24 @@
+from bst import *
+from hmstr import *
+import sys
+
+
+def input_data(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            number_of_humsters = int(file.readline()) #S
+            amount_of_eat = int(file.readline()) #C
+            humster_bst = BST(lambda x: (x.avarice, x.daily_norm))
+            for line in file.readlines():
+                daily_norm, avarice = line.split()
+                humster_bst.add(Hamster(int(daily_norm), int(avarice)))
+        return number_of_humsters, amount_of_eat, humster_bst
+    except FileNotFoundError:
+        sys.exit(f'file "{file_name} not found')
+    except:
+        sys.exit(f'check data in "{file_name}"')
+
+
+def output_data(file_name, value_to_write):
+    with open(file_name, 'w') as file:
+        file.write(value_to_write)
---
+++ main.py
@@ -0,0 +1,31 @@
+from io_data import *
+
+
+def main(file_in):
+    number_of_humsters, amount_of_eat, hamsters_bst = input_data(file_in)
+
+    checked_humsters = 0
+    total_avarice = 0
+    total_daily_norm = 0
+
+    while checked_humsters < amount_of_eat:
+        current = hamsters_bst.get_min_element()
+        total_avarice += current.avarice
+        total_daily_norm += current.daily_norm
+        checked_humsters += 1
+
+        if total_daily_norm + total_avarice * checked_humsters > number_of_humsters:
+            break
+
+        hamsters_bst.remove_min_element()
+    return checked_humsters
+
+
+if __name__ == "__main__":
+    first_file_in = 'IO/hamster1.in'
+    first_file_out = 'IO/hamster1.out'
+    first_result = main(first_file_in)
+    
+    print(f'First hamsters result: {first_result}')
+
+    output_data(first_file_out, str(first_result))
